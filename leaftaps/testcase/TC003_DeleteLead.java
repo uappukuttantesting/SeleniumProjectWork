10c29758e6560e231a78a0d145cc90aa80233489
uappukuttantesting <56801036+uappukuttantesting@users.noreply.github.com>
2019-12-31 06:06:05 +0000

    Updated Coe

--- leaftaps/testcase/TC003_DeleteLead.java
+++ leaftaps/testcase/TC003_DeleteLead.java
@@ -5,16 +5,16 @@ import org.testleaf.leaftaps.pages.LoginPage;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class TC002_EditLead extends BaseClass {
-
+public class TC003_DeleteLead extends BaseClass{
+	
 	@BeforeTest
 	public void setData() {
 		excelFileName = "TC002";
 
 	}
-
+	
 	@Test(dataProvider = "excelData")
-	public  void runTC002(String username, String password, String compname, String firstName, String lastName) throws InterruptedException {
+	public  void runTC003(String username, String password, String compname, String firstName, String lastName) throws InterruptedException {
 
 		new LoginPage()
 		.enterUsername(username)
@@ -28,9 +28,10 @@ public class TC002_EditLead extends BaseClass {
 		.enterLastName(lastName)
 		.clickCreateLead()
 		.clickFindLeads()
-		.enterLeadIdEdit()
-		.updateCompName()
-		.clickUpdateButton();
+		.enterLeadIdDelete()
+		.clickDeleteButton()
+		.clickFindLeads()
+		.enterLeadIdDelSearch();
 
 	}
 
